398fbeb8941361bb1edfbd3cd14dff6adbb1e836
novans123 <novansetyawan.ns28@gmail.com>
2022-11-20 20:37:45 +0700

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>MANTAAP</Text>
+      <Text>MANTAAP slurr</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +15,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'left',
     justifyContent: 'center',
   },
 });
